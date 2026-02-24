5df5e961a5f1b4fb07c04ac533e307751914b7e1
renisson01 <renisson@proton.me>
2026-02-24 12:43:21 +0000

    Add: Web scraper for all Brazil races

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import { racesRoutes } from './modules/races/races.routes.js';
 import { resultsRoutes } from './modules/results/results.routes.js';
+import { scraperRoutes } from './modules/scraper/scraper.routes.js';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -14,13 +15,9 @@ const app = Fastify({ logger: true });
 await app.register(cors, { origin: '*' });
 await app.register(racesRoutes);
 await app.register(resultsRoutes);
+await app.register(scraperRoutes);
 
 app.get('/', async (request, reply) => {
-  const html = fs.readFileSync(path.join(__dirname, '../public/index.html'), 'utf-8');
-  reply.type('text/html').send(html);
-});
-
-app.get('/index.html', async (request, reply) => {
   const html = fs.readFileSync(path.join(__dirname, '../public/index.html'), 'utf-8');
   reply.type('text/html').send(html);
 });
